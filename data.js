0278af3fcf0632fd4ea7a19125c2a3d649adebb8
zhenghanjing <zhenghanjing@kuaishou.com>
2022-11-14 10:56:34 +0800

    feat: 完成目标

---
+++ data.js
@@ -0,0 +1,11 @@
+exports.allStr = `《扬名立万》《Be Somebody》《这个杀手不太冷静》《Too Cool To Kill》
+tt8323668《光年正传》《Lightyear》
+tt10298810《魔女2》《The Witch: Part 2. The Other One》
+tt13721828《没有养老的资金》《I Don't Have Any Money Left in My Retirement Account》
+tt11354164《犯罪都市》《The Roundup》
+tt7468056《犯罪都市2》《The Roundup2》
+tt15838850《海兽猎人》《The Sea Beast》
+tt9288046《独行月球》《Moon Man》
+tt14557302《灰影人》《The Gray Man》《小黄人大眼萌：神偷奶爸前传》《Minions: The Rise of Gru》
+tt51130442022年08月01日
+`
