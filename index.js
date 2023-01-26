eb251eea9ba768b857720f905330daa8102d66af
rexkentzheng <331749897@qq.com>
2023-01-26 11:11:54 +0800

    feat: :sparkles:支持通过IMDB的ID来拉取拼接海报

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const download = require('download')
 const im = require("imagemagick")
 const ImgConcat = require('@jyeontu/img-concat');
 const data = require('./data.js')
-const imageBaseUrl = 'https://image.tmdb.org/t/p/original/'
-
-// curl https://api.themoviedb.org/3/movie/tt1016150?api_key=1b311eadd5f9863714486717ba314840&language=zh-CN
-
-// https://api.themoviedb.org/3/movie/76341?api_key=1b311eadd5f9863714486717ba314840&language=de
+const idList = require('./ids.js')
+const imageBaseUrl = 'https://image.tmdb.org/t/p/original'
 
 const arguments = process.argv;
 const ImgConcatClass = new ImgConcat();
@@ -15,10 +12,12 @@ const getContent = RegExp(/(?<=《).*?(?=》)/g)
 const isChinese = new RegExp("[\u4E00-\u9FA5]+");
 
 const names = [...data.allStr.matchAll(getContent)].filter(item => isChinese.test(item[0])).map(item => item[0])
-const errNames = []
 
 
-const fillPosters = () => {
+const ids = idList.ids.split('\n').filter(id => !!id)
+
+const fillPosters = (names) => {
+	const errNames = []
 	return Promise.all(names.map((name) => {
 		return new Promise((reslove, reject) => {
 			return request.get({
@@ -30,7 +29,7 @@ const fillPosters = () => {
 					reslove()
 				} else {
 					return download(
-						`https://image.tmdb.org/t/p/original${results[0].poster_path}`,
+						`${imageBaseUrl}${results[0].poster_path}`,
 						'./posters',
 						{
 							filename: `${name}.jpg`
@@ -43,35 +42,63 @@ const fillPosters = () => {
 			})
 		})
 	})).then(() => {
-	if (!errNames.length) {
-		console.log(`无法找到的影片为：${errNames}`);
-	}
-});
+		if (!errNames.length) {
+			console.log(`无法找到的影片为：${errNames}`);
+		}
+		console.log('海报下载完成')
+	});
 }
 
-const joinImgs = () => {
+const fillPostersByID = (ids) => {
+	console.log('开始拉取海报！')
+	return Promise.all(ids.map((id) =>
+		new Promise((reslove) => {
+			request.get({
+				url: encodeURI(`https://api.themoviedb.org/3/movie/${id}?api_key=1b311eadd5f9863714486717ba314840&language=en-US`)
+			}, (err, res, body) => {
+				const results = JSON.parse(body)
+				const { poster_path, title } = results
+				return download(
+					`${imageBaseUrl}${poster_path}`,
+					'./posters',
+					{
+						filename: `${id}.jpg`
+					}
+				).then(() => {
+					console.log(`${id}.jpg下载成功 ${title}`);
+					reslove()
+				})
+			})
+		})
+	)).then(() => {
+		console.log('海报下载完成')
+	})
+}
+
+const joinImgs = (names) => {
+	console.log('开始拼接海报', names)
 	const finalArr = []
 	let helper = [];
 	names.forEach((name, index) => {
 		helper.push(`./posters/${name}.jpg`);
-			if (helper.length === 5) {
-				finalArr.push(JSON.parse(JSON.stringify(helper)));
-				helper = []
-			} else if (index === names.length - 1) {
-				finalArr.push(JSON.parse(JSON.stringify(helper)));
-			}
-		})
+		if (helper.length === 5) {
+			finalArr.push(JSON.parse(JSON.stringify(helper)));
+			helper = []
+		} else if (index === names.length - 1) {
+			finalArr.push(JSON.parse(JSON.stringify(helper)));
+		}
+	})
 
 	ImgConcatClass.conCatByMaxit({
 		shape: finalArr,
 		target: './output/'
-	}).then(res=>{
-    console.log(`拼接完成,图片路径为${res}，开始压缩`);
-    console.log('压缩过程较久，请耐心等待');
-    im.resize({
-		  srcPath: res,
-		  dstPath: './output/tada.jpg',
-		  width:3000,
+	}).then(res => {
+		console.log(`拼接完成,图片路径为${res}，开始压缩`);
+		console.log('压缩过程较久，请耐心等待');
+		im.resize({
+			srcPath: res,
+			dstPath: './output/tada.jpg',
+			width:3000,
 		}, (err, stdout, stderr) => {
 			if (err) {
 				console.log(`压缩失败：${err}`)
@@ -84,24 +111,29 @@ const joinImgs = () => {
 
 const main = ([_node_path, _file_path, type]) => {
 	if (!type) {
-		return console.log('请传入必传参数，1为下载海报，2为拼接海报')
+		return console.log('请传入必传参数，1为通过ID下载海报，2为通过名称下载海报，3为通过ID拼接海报，4为通过名称拼接海报')
 	}
 	if (+type === 1) {
-		console.log('开始下载海报')
-		fillPosters()
+		fillPostersByID(ids)
+	} else if (+type === 2) {
+		fillPosters(names)
+	} else if (+type === 3) {
+		joinImgs(ids)
+	} else if (+type === 4) {
+		joinImgs(names)
 	} else {
-		console.log('开始拼接海报')
-		joinImgs()
+		// 防止图片过大做的冗余处理
+		ImgConcatClass.collapseVertical({
+			left: './output/tada1.jpg',
+			right: './output/tada.jpg',
+			target:'./output/'
+		}).then(res=>{
+			console.log(`拼接完成,图片路径为${res}`);
+		});
 	}
 }
 
 main(arguments);
-
-
-
-
-
-
 
 
 
