f32f0cb72e1c572939564122ecc5b9fff8721a1c
zhenghanjing <zhenghanjing@kuaishou.com>
2023-01-26 11:11:54 +0800

    feat: :sparkles:支持通过IMDB的ID来拉取拼接海报

--- data.js
+++ data.js
@@ -1,11 +1,332 @@
-exports.allStr = `《扬名立万》《Be Somebody》《这个杀手不太冷静》《Too Cool To Kill》
-tt8323668《光年正传》《Lightyear》
-tt10298810《魔女2》《The Witch: Part 2. The Other One》
-tt13721828《没有养老的资金》《I Don't Have Any Money Left in My Retirement Account》
-tt11354164《犯罪都市》《The Roundup》
-tt7468056《犯罪都市2》《The Roundup2》
-tt15838850《海兽猎人》《The Sea Beast》
-tt9288046《独行月球》《Moon Man》
-tt14557302《灰影人》《The Gray Man》《小黄人大眼萌：神偷奶爸前传》《Minions: The Rise of Gru》
-tt51130442022年08月01日
+exports.allStr = `《扬名立万》《Be Somebody》
+——刘循子墨				2022年02月05日
+《这个杀手不太冷静》《Too Cool To Kill》
+——邢文雄					2022年02月07日
+《误杀2》《Fireflies in the Sun》
+——戴墨					2022年02月08日
+《枕刀歌之尘世行》
+——薛锦胜 / 阮志豪		    2022年02月16日
+《王牌特工：源起》《The King's Man》
+——马修·沃恩				2022年02月26日
+《欢喜冤家》《The Hating Game》
+——彼得·哈钦斯				2022年02月26日
+《纽约的一个雨天》《A Rainy Day in New York》
+——伍迪·艾伦				2022年03月03日
+《银行家》《The Banker》
+——乔治·诺非				2022年03月07日
+《幽冥》《Spectral》
+——尼克·马蒂厄				2022年03月09日
+《星际穿越》《Interstellar》
+——克里斯托弗·诺兰			2022年03月12日
+《蜘蛛侠：英雄无归》《Spider-Man: No Way Home》
+——乔·沃茨					2022年03月13日
+《青春变形记》《Turning Red》
+——石之予					2022年03月18日
+《指环王1：护戒使者》《The Lord of the Rings: The Fellowship of the Ring》
+——彼得·杰克逊				2022年03月19日
+《指环王2：双塔奇兵》《The Lord of the Rings: The Two Towers》
+——彼得·杰克逊				2022年03月28日
+《指环王3：王者无敌》《The Lord of the Rings: The Return of the King》
+——彼得·杰克逊				2022年04月05日
+《扫黑·决战》《Break Through the Darkness》
+——吕聿来					2022年04月09日
+《火星救援》《The Martian》
+——雷德利·斯科特			2022年04月09日
+《神奇动物：邓布利多之谜》《Fantastic Beasts: The Secrets of Dumbledore》
+——大卫·叶茨				2022年04月17日
+《暖暖内含光》《Eternal Sunshine of the Spotless Mind》
+——米歇尔·贡德里			2022年04月23日
+《复仇者联盟》《The Avengers》
+——乔斯·韦登				2022年04月29日
+《复仇者联盟2：奥创纪元》《Avengers: Age of Ultron》
+——乔斯·韦登				2022年04月30日
+《东北告别天团》《Goodbye》
+——崔志佳					2022年05月01日
+《复仇者联盟3：无限战争》《Avengers: Infinity War》
+——安东尼·罗素 / 乔·罗素	2022年05月02日
+《复仇者联盟4：终局之战》《Avengers: Endgame》
+——安东尼·罗素 / 乔·罗素	2022年05月03日
+《神秘海域》《Uncharted》
+——鲁本·弗雷斯彻			2022年05月03日
+《新蝙蝠侠》《The Batman》
+——马特·里夫斯				2022年05月04日
+《机器人总动员》《WALL·E》
+——安德鲁·斯坦顿			2022年05月13日
+《野性的呼唤》《The Call of the Wild》
+——克里斯·桑德斯			2022年05月14日
+《瞬息全宇宙》《Everything Everywhere All at Once》
+——关家永 / 丹尼尔·施纳特	2022年05月20日
+《暗夜博士：莫比亚斯》《Morbius》
+——丹尼尔·伊斯皮诺萨		2022年05月23日
+《疯狂的外星人》《Crazy Alien》
+——宁浩					2022年05月30日
+《奇奇与蒂蒂：救援突击队》《Chip 'n' Dale: Rescue Rangers》
+——阿吉瓦·沙弗尔			2022年06月01日
+《套装》《The Outfit》
+——格拉汉姆·摩尔			2022年06月03日
+《The Bad Guys》《坏蛋联盟》
+——彼埃尔·佩里菲尔			2022年06月11日
+《天才不能承受之重》《The Unbearable Weight of Massive Talent》
+——汤姆·戈米肯				2022年06月19日
+《必胜球探》《Hustle》
+——杰里米·撒加				2022年6月20日
+《奇异博士2：疯狂多元宇宙》《Doctor Strange in the Multiverse of Madness》
+——山姆·雷米				2022年06月22日
+《祝你好运，里奥·格兰德》《Good Luck to You, Leo Grande》
+——苏菲·海德				2022年06月25日
+《007：大战皇家赌场》《Casino Royale》
+——马丁·坎贝尔				2022年06月26日
+《双子杀手》《Gemini Man》
+——李安					2022年07月02日
+《三个老枪手》《Going in Style》
+——扎克·布拉夫				2022年07月03日
+《剑雨》《Reign of Assassins》
+——苏照彬					2022年07月05日
+《狮子王》《The Lion King》
+——乔恩·费儒				2022年07月08日
+《侏罗纪世界3》《Jurassic World: Dominion》
+——科林·特莱沃若			2022年07月09日
+《杰瑞和玛姬生活阔绰》《Jerry and Marge Go Large》
+                                    tt8323668
+——大卫·弗兰科尔			2022年07月12日
+《光年正传》《Lightyear》
+                                    tt10298810
+——安格斯·麦克莱恩			2022年07月17日
+《魔女2》《The Witch: Part 2. The Other One》
+                                    tt13721828
+——朴勋政					2022年07月19日
+《没有养老的资金》《I Don't Have Any Money Left in My Retirement Account》
+                                    tt11354164
+——前田哲					2022年07月21日
+《犯罪都市》《The Roundup》
+                                    tt7468056
+——姜允成					2022年07月25日
+《犯罪都市2》《The Roundup2》
+                                    tt15838850
+——李相龙					2022年07月25日
+《海兽猎人》《The Sea Beast》
+                                    tt9288046
+——克里斯·威廉姆斯			2022年07月29日
+《独行月球》《Moon Man》
+                                    tt14557302
+——张吃鱼						2022年07月30日
+《灰影人》《The Gray Man》
+——安东尼·罗素 / 乔·罗素		2022年07月30日
+《小黄人大眼萌：神偷奶爸前传》《Minions: The Rise of Gru》
+                                    tt5113044
+——凯尔·巴尔达 / 布拉德·埃布尔森 / 乔纳森·德尔瓦尔
+                            2022年08月01日
+ 
+《再次出发之纽约遇见你》《Begin Again》
+                                    tt1980929
+——约翰·卡尼					2022年08月04日
+《明日战记》《Warriors of Future》
+                                    tt7375466
+——吴炫辉						2022年08月06日
+《酒精计划》《Another Round》
+                                    tt10288566
+——托马斯·温特伯格				2022年08月06日
+《疾速追杀》《John Wick》
+                                    tt2911666
+——大卫·雷奇 / 查德·斯塔赫斯基	2022年08月06日
+《秒速5厘米》
+                                    tt0983213
+——新海诚						2022年08月13日
+《铁血战士：猎物》《Prey》
+                                    tt11866324
+——丹·特拉亨伯格				2022年08月14日
+《钢的琴》《The Piano in a Factory》
+                                    tt1723118
+——张猛						2022年08月16日
+《简爱》《Jane Eyre》
+                                    tt1229822
+——凯瑞·福永					2022年08月17日
+《日班猎人》《Day Shift》
+                                    tt13314558
+——J·J·佩里						2022年08月19日
+《敢死队》《The Expendables》
+                                    tt1320253
+——西尔维斯特·史泰龙			2022年08月20日
+《壮志凌云》《Top Gun》
+                                    tt0092099
+——托尼·斯科特					2022年08月24日
+《壮志凌云2：独行侠》《Top Gun: Maverick》
+                                    tt1745960
+——约瑟夫·科辛斯基				2022年08月24日
+《撒玛利亚》《Samaritan》
+                                    tt5500218
+——朱利叶斯·艾弗里				2022年08月27日
+《黑客帝国》《The Matrix》
+                                    tt0133093
+——莉莉·沃卓斯基 / 拉娜·沃卓斯基
+                            2022年09月03日
+《黑客帝国2：重装上阵》《The Matrix Reloaded》
+                                    tt0234215
+——拉娜·沃卓斯基 / 莉莉·沃卓斯基
+                            2022年09月03日
+《黑客帝国3：矩阵革命》《The Matrix Revolutions》
+                                    tt0242653
+——拉娜·沃卓斯基 / 莉莉·沃卓斯基
+                            2022年09月03日
+《黑客帝国动画版》《The Animatrix》
+                                    tt0328832
+——钟彼得 / 安德鲁·R·琼斯 / 川尻善昭 / 小池健 / 前田真宏 / 森本晃司 / 渡边信一郎								2022年09月04日
+《坠落》《Fall》
+                                    tt15325794
+——斯科特·曼					2022年09月04日
+《沼泽深处的女孩》《Where the Crawdads Sing》
+                                    tt9411972
+——奥利维亚·纽曼				2022年09月06日
+《雷神4：爱与雷霆》《Thor: Love and Thunder》
+                                    tt10648342
+——塔伊加·维迪提				2022年09月08日
+《无名之辈》《A Cool Fish》
+                                    tt9282616
+——饶晓志						2022年09月10日
+《捕蝇纸》《Flypaper》
+                                    tt1541160
+——罗伯·明可夫					2022年09月11日
+《第五元素》《The Fifth Element》
+                                    tt0119116
+——吕克·贝松					2022年09月18日
+《阿修罗》《Asura: The City of Madness》
+                                    tt5918028
+——金成洙						2022年09月25日
+《Jose与虎与鱼们》《Josée》
+                                    tt13411298
+——金宗宽						2022年10月01日
+《金馆长对金馆长对金馆长》《Mr. Kim Vs. Mr. Kim Vs. Mr. Kim》
+                                    tt0984057
+——朴成均						2022年10月02日
+《子弹列车》《Bullet Train》
+                                    tt12593682
+——大卫·雷奇					2022年10月10日
+《共助》《Confidential Assignment》
+                                    tt5606538
+——金成勋						2022年10月18日
+《哥们儿》《Bros》
+                                    tt9731598
+——尼古拉斯·斯托勒				2022年10月23日
+《有史以来最棒的啤酒运送》《The Greatest Beer Run Ever》
+                                    tt10268488
+——彼得·法雷里					2022年10月27日
+《赤焰战场》《Red》
+                                    tt1245526
+——罗伯特·斯文克				2022年10月29日
+《共助2：国际》《Confidential Assignment 2: International》
+                                    tt21867370
+——梨晳勳						2022年11月02日
+《西线无战事》
+                                    tt1016150
+——爱德华·贝尔格					2022年11月05日
+《新神榜：杨戬》《New Gods: Yang Jian》
+                                    tt19757204
+——赵霁						2022年11月09日
+《新神榜：哪吒重生》《New Gods: Nezha Reborn》
+                                    tt13269670
+——赵霁						2022年11月12日
+《当男人恋爱时》《When a Man Loves a Woman》
+                                    tt3479250
+——韩东旭						2022年11月14日
+《狩猎》《Hunt》
+                                    tt6931658
+——李政宰						2022年11月16日
+《名侦探柯南：万圣节的新娘》《Detective Conan: The Bride of Halloween》
+                                    tt19770970
+——满仲劝						2022年11月18日
+《铁雨》《Steel Rain》
+                                    tt6769508
+——杨宇硕						2022年11月20日
+《看他们如何逃之夭夭》《See How They Run》
+                                    tt13640696
+——汤姆·乔治					2022年11月22日
+《黑亚当》《Black Adam》
+                                    tt6443346
+——佐米·希尔拉					2022年11月24日
+《悲情三角》《Triangle of Sadness》
+                                    tt7322224
+——鲁本·奥斯特伦德				2022年11月26日
+《梦乡》《Slumberland》
+                                    tt13320662
+——弗朗西斯·劳伦斯				2022年11月26日
+《恐怖直播》《The Terror Live》
+                                    tt2990738
+——金秉祐						2022年11月28日
+《分手的决心》《Decision to Leave》
+                                    tt12477480
+——朴赞郁						2022年11月30日
+《布达佩斯大饭店》《The Grand Budapest Hotel》
+                                    tt2278388
+——韦斯·安德森					2022年12月03日
+《山怪巨魔》《Troll》
+                                    tt11116912
+——罗阿尔·乌索格				2022年12月04日
+《赤焰战场2》《Red: The Legend》
+                                    tt1821694
+——迪恩·帕里索					2022年12月06日
+《秘密特工》《The Man from U.N.C.L.E.》
+                                    tt1638355
+——盖·里奇						2022年12月07日
+《老师，您能坐在我旁边吗？》《Teacher, Please Sit Next to Me》
+                                    tt14786640
+——堀江贵大					2022年12月10日
+《解放黑奴》《Emancipation》
+                                    tt12530246
+——安东尼·福奎阿				2022年12月12日
+《吉尔莫·德尔·托罗的匹诺曹》《Guillermo Del Toro's Pinocchio》
+                                    tt1488589
+——吉尔莫·德尔·托罗 / 马克·古斯塔夫森
+                            2022年12月13日
+《白日梦想家》《The Secret Life of Walter Mitty》
+                                    tt0359950
+——本·斯蒂勒					2022年12月15日
+《兰博基尼》《Lamborghini》
+                                    tt5533370
+——罗伯特·莫里斯克				2022年12月18日
+《伊尼舍林的报丧女妖》《The Banshees of Inisherin》
+                                    tt11813216
+——马丁·麦克唐纳				2022年12月19日
+《万里归途》《Home Coming》
+                                    tt22049410
+——饶晓志						2022年12月20日
+《达拉斯买家俱乐部》《Dallas Buyers Club》
+                                    tt0790636
+——让-马克·瓦雷					2022年12月20日
+《战争之王》《Lord of War》
+                                    tt0399295
+——安德鲁·尼科尔				2022年12月21日
+《暴力之夜》《Violent Night》
+                                    tt12003946
+——托米·维尔科拉				2022年12月22日
+《利刃出鞘2》《Glass Onion: A Knives Out Mystery》
+                                    tt11564570
+——莱恩·约翰逊					2022年12月25日
+《骗子》《The Swindlers》
+                                    tt7243686
+——张昌原						2022年12月31日
+《疾速追杀2》《John Wick: Chapter 2》
+                                    tt4425200
+——查德·斯塔赫斯基				2023年01月01日
+《奎迪》《Creed》
+                                    tt3076658
+——瑞恩·库格勒					2023年01月02日
+《疾速追杀3》《John Wick: Chapter 3 - Parabellum》
+                                    tt6146586
+——查德·斯塔赫斯基				2023年01月02日
+《关山飞渡》《Stagecoach》
+                                    tt0031971
+——约翰·福特					2023年01月07日
+《洛奇》《Rocky》
+                                    tt0075148
+——约翰·G·艾维尔森				2023年01月10日
+《小鞋子》《Children of Heaven》
+                                    tt0118849
+——马基德·马基迪				2023年01月16日
+《阿凡达》《Avatar》
+                                    tt0499549
+——詹姆斯·卡梅隆				2023年01月17日
+《阿凡达：水之道》《Avatar: The Way of Water》
+                                    tt1630029
+——詹姆斯·卡梅隆				2023年01月18日
+
 `
